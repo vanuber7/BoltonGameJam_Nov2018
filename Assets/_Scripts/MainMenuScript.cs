b710ff0389553e41240fe024d06afeece33c832a
Andrew Wilson <am-wilson@hotmail.co.uk>
2018-11-09 17:05:17 +0000

    Added Ben main menu and credits menu. Added Textmesh pro package

---
+++ Assets/_Scripts/MainMenuScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public void PlayGame ()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void MainMenu ()
+	{
+		SceneManager.LoadScene(0);
+	}
+
+	public void CreditsScreen ()
+	{
+		SceneManager.LoadScene(2);
+	}
+
+	public void GameSettings ()
+	{
+		SceneManager.LoadScene(3);
+	}
+}
